6615f2a0f7093df885d965f6b40a8ae2633ca4dc
Emily Price <erp35@uclive.ac.nz>
2016-09-20 16:17:28 +1200

    Adds a listener so that when a new term is selected some info about it is displayed
    Story: 692

--- src/main/java/seng302/gui/TermsSettingsController.java
+++ src/main/java/seng302/gui/TermsSettingsController.java
@@ -1,11 +1,13 @@
 package seng302.gui;
 
 import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXTextArea;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import seng302.Environment;
 import seng302.data.Term;
@@ -18,6 +20,12 @@ public class TermsSettingsController {
     @FXML
     private JFXListView termsListView;
 
+    @FXML
+    private Label selectedName;
+
+    @FXML
+    private JFXTextArea selectedDefinition;
+
     private Environment env;
 
     public void create(Environment env) {
@@ -27,6 +35,22 @@ public class TermsSettingsController {
         List<String> termNames = this.env.getMttDataManager().getTerms().stream().map(Term::getMusicalTermName).collect(Collectors.toList());
 
         termsListView.getItems().setAll(termNames);
+
+        termsListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            // Display musical term with newvalue
+            selectedName.setText(newValue.toString());
+
+            Term selectedTerm;
+
+            for (Term term : env.getMttDataManager().getTerms()) {
+                if (term.getMusicalTermName().equals(newValue.toString())) {
+                    selectedTerm = term;
+                    selectedDefinition.setText(selectedTerm.getMusicalTermDefinition());
+                }
+            }
+
+
+        });
     }
 
 }
