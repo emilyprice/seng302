ed532d1cfd615506ba0850b36e5aee046bd15108
Emily Price <erp35@uclive.ac.nz>
2016-09-20 15:46:03 +1200

    Adds a JFX List View that displays the names of all the current musical terms.
    Story: 692

--- src/main/java/seng302/gui/TermsSettingsController.java
+++ src/main/java/seng302/gui/TermsSettingsController.java
@@ -1,19 +1,32 @@
 package seng302.gui;
 
+import com.jfoenix.controls.JFXListView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import seng302.Environment;
+import seng302.data.Term;
 
 public class TermsSettingsController {
 
     @FXML
     private AnchorPane termsSettingsAnchor;
 
+    @FXML
+    private JFXListView termsListView;
+
     private Environment env;
 
     public void create(Environment env) {
         this.env = env;
         env.getRootController().setHeader("Musical Term Settings");
+
+        List<String> termNames = this.env.getMttDataManager().getTerms().stream().map(Term::getMusicalTermName).collect(Collectors.toList());
+
+        termsListView.getItems().setAll(termNames);
     }
 
 }
