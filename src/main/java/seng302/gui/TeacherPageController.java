d075bc54c9fa7705f0154ad7889cfdfa258d8e54
Emily Price <erp35@uclive.ac.nz>
2016-09-23 17:11:38 +1200

    Implements skeleton code of displaying summary pages and user summary pages.
    Story: 719

--- src/main/java/seng302/gui/TeacherPageController.java
+++ src/main/java/seng302/gui/TeacherPageController.java
@@ -1,7 +1,7 @@
 package seng302.gui;
 
-import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -12,16 +12,18 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import seng302.Environment;
 
+import java.util.ArrayList;
+
 public class TeacherPageController {
 
     @FXML
     private VBox profilepic;
 
     @FXML
-    private JFXListView<?> listView;
+    private JFXListView listView;
 
-    @FXML
-    private JFXButton btnSettings;
+//    @FXML
+//    private JFXButton btnSettings;
 
     @FXML
     private Label txtFullName;
@@ -54,5 +56,58 @@ public class TeacherPageController {
 
     public void load() {
         imageDP2.setImage(env.getUserHandler().getCurrentUser().getUserPicture());
+        populateUserOptions();
+    }
+
+
+    public void populateUserOptions() {
+
+        ArrayList<String> options = new ArrayList<>();
+        options.add("Summary");
+        options.add("Test User");
+
+        listView.getItems().addAll(FXCollections.observableArrayList(options));
+
+        listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            showPage((String) newValue);
+        });
+
+
+        listView.setMaxWidth(200);
+        listView.setMinWidth(200);
+        listView.setDepthProperty(1);
+
+
+//        listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+//            showPage((String) newValue);
+//        });
+
+        // Set after the listener so it loads user summary correctly
+        listView.getSelectionModel().selectFirst();
+
+
+    }
+
+    public void showPage(String pageName) {
+
+//        setupTimeSlider();
+        if (pageName.equals("Summary")) {
+            showSummaryPage();
+        } else {
+            showUserPage(pageName);
+        }
+
+
+    }
+
+    public void showSummaryPage() {
+        env.getRootController().setHeader("Summary");
+
+
+    }
+
+    public void showUserPage(String userName) {
+        env.getRootController().setHeader("Student - " + userName);
+
     }
 }
